fd3b4b8009b51971967a0d720552f9c6562593e0
h0ldnack <git@mail.holdnack.net>
2022-05-17 15:08:34 -0700

    Automatically added on <2022-05-17T15-07:00>

--- build.sh
+++ build.sh
@@ -2,23 +2,24 @@
 #! nix-shell -i fish -p pkgs.pandoc pkgs.python39Packages.weasyprint
 
 function build-markdown
-	pandoc index.html -f html -t markdown -s -o readme.md
+	pandoc index.html -f html -t markdown -s --css=style.css -o readme.md
 end
 
 function build-pdf
-	pandoc index.html -f html -t pdf --pdf-engine=weasyprint -s -o resume.pdf
+	pandoc index.html -f html -t pdf --pdf-engine=weasyprint --css=style.css -s -o resume.pdf
 end
 
 function sync
 	set DATE (date --iso-8601=hours)
 	set MSG "Automatically added on <$DATE>"
+	git pull
 	git add *
 	git commit -m "$MSG"
 	git push -u origin main
 end
 
 function main
-	build-markdown
+	#build-markdown
 	build-pdf
 	sync
 end
