fdf44ff76ee27668afb0895f88e5ae81d4ae98d2
h0ldnack <git@mail.holdnack.net>
2022-05-17 22:17:18 -0700

    Automatically added on <2022-05-17T22-07:00>

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i fish -p pkgs.pandoc pkgs.python39Packages.weasyprint
+#! nix-shell -i fish -p pkgs.pandoc pkgs.python39Packages.weasyprint pkgs.entr
 
 
 function build-pdf
