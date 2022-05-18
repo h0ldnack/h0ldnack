e0f42df00100f0a5fef027635d8aaa88ab647aaf
h0ldnack <git@mail.holdnack.net>
2022-05-17 21:31:48 -0700

    Automatically added on <2022-05-17T21-07:00>

--- web-worker.js
+++ web-worker.js
@@ -1,4 +1,31 @@
 caches.open("pwa-assets")
 .then(cache => {
-	cache.addAll(["core.css", "webstyle.css", "resume.html", "index.html", "web-worker.js", "manifest.json"]); 
+	cache.addAll(); 
+});
+
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+      caches.open('fox-store').then((cache) => cache.addAll(
+		  ["core.css",
+		   "webstyle.css",
+		   "resume.html",
+		   "index.html",
+		   "web-worker.js",
+		   "manifest.json",
+		   "images/android-chrome-192x192.png",
+		   "images/apple-touch-icon.png",
+		   "images/favicon-32x32.png",
+		   "images/android-chrome-512x512.png",
+		   "images/favicon-16x16.png",
+		   "images/favicon.ico"]
+   )),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
 });
